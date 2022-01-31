556b4dcbe2ea3ce658ca94e0da423b11e13e0916
Rhys Huxley <rhyshuxley@gmail.com>
2022-01-31 18:38:05 +0000

    Calculate personal price using -s flag

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import discord
 import requests
 import json
+import re
 
 TOKEN = os.environ['TOKEN']
 APIKEY = os.environ['APIKEY']
@@ -10,7 +11,7 @@ client = discord.Client()
 def get_price():
   response = requests.get('https://api.twelvedata.com/price?symbol=AMC&apikey=' + APIKEY)
   data = json.loads(response.content)
-  return(data['price'])
+  return(float(data['price']))
 
 @client.event
 async def on_ready():
@@ -22,6 +23,12 @@ async def on_message(message):
     return
 
   if message.content.startswith('$amc'):
-    await message.channel.send('$' + get_price())
+    if '-s' in message.content:
+      stocks = re.findall(r'\d+', message.content)
+      price = get_price() * int(stocks[0])
+    else:
+      price = get_price()
+
+    await message.channel.send('$' + "{:.2f}".format(price))
 
 client.run(TOKEN)
