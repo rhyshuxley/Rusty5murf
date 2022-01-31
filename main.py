599367688c3a67c3fbb64af9f55aff4b1310ea78
Rhys Huxley <rhyshuxley@gmail.com>
2022-01-31 17:24:15 +0000

    Fix typo

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.authro == client.user:
+  if message.author == client.user:
     return
 
   if message.content.startswith('$hello'):
