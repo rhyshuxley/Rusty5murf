5ce420a23b752c6bbe8e9555019fbeacb0a588aa
Rhys Huxley <rhyshuxley@gmail.com>
2022-01-31 17:22:39 +0000

    Fix ready message

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-  print('{0.user}.format(client) is ready!')
+  print('{0.user}'.format(client) + 'is ready!')
 
 @client.event
 async def on_message(message):
