128b662199beb4a96fc59d3be41e4f1ab8fe3cd9
Rhys Huxley <rhyshuxley@gmail.com>
2022-01-31 17:18:11 +0000

    Add on_message

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ client = discord.Client()
 @client.event
 async def on_ready():
   print('{0.user}.format(client) is ready!')
+
+@client.event
+async def on_message(message):
+  if message.authro == client.user:
+    return
+
+  if message.content.startswith('$hello')
+    await message.channel.send('Hey!')
