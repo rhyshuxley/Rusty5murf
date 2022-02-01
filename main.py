c749d64926b7ad98cce40738932e1c2408b3c651
Rhys Huxley <rhyshuxley@gmail.com>
2022-02-01 21:33:14 +0000

    Update to use any symbol and amount

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ import os
 import discord
 import requests
 import json
-import re
 from keep_alive import keep_alive
 
 TOKEN = os.environ['TOKEN']
 APIKEY = os.environ['APIKEY']
 client = discord.Client()
 
-def get_price(symbol):
+def get_price(symbol, amount):
   response = requests.get('https://api.twelvedata.com/price?symbol=' + symbol + '&apikey=' + APIKEY)
   data = json.loads(response.content)
-  return(float(data['price']))
+  return(float(data['price']) * float(amount))
 
 @client.event
 async def on_ready():
@@ -23,23 +22,16 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$amc'):
-    if '-s' in message.content:
-      stocks = re.findall(r'\d+', message.content)
-      price = get_price('AMC') * int(stocks[0])
+  if message.content.startswith('$stock'):
+    args = message.content.split()
+    symbol = args[1]
+
+    if len(args) == 3:
+      amount = args[2]
     else:
-      price = get_price('AMC')
+      amount = 1
 
-    await message.channel.send('$' + "{:.2f}".format(price))
-
-  if message.content.startswith('$gme'):
-    if '-s' in message.content:
-      stocks = re.findall(r'\d+', message.content)
-      price = get_price('GME') * int(stocks[0])
-    else:
-      price = get_price('GME')
-
-    await message.channel.send('$' + "{:.2f}".format(price))
+    await message.channel.send('$' + "{:.2f}".format(get_price(symbol, amount)))
 
 keep_alive()
 client.run(TOKEN)
